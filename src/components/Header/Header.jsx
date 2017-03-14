7a1019663ec39e312f4b463f0edf99533e8450bf
Nort <4knort@gmail.com>
2017-03-14 19:33:14 +0200

    little style to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,19 @@ import './header.scss';
 
 const Header = () => (
   <header className="header clearfix">
-    <a href="" className="logo">
-      <img src="" alt="" />
-    </a>
-    <nav className="header__nav">
-      <li className="header__nav-item">
-        <IndexLink to={'/'} activeClassName="header__nav-link--active" className="header__nav-link">Create</IndexLink>
-      </li>
-      <li className="header__nav-item">
-        <Link to={'/explore'} activeClassName="header__nav-link--active" className="header__nav-link">Explore</Link>
-      </li>
-    </nav>
+    <div className="container">
+      <a href="" className="logo">
+        <img src="" alt="" />
+      </a>
+      <nav className="header__nav">
+        <li className="header__nav-item">
+          <IndexLink to={'/'} activeClassName="header__nav-link--active" className="header__nav-link">Create</IndexLink>
+        </li>
+        <li className="header__nav-item">
+          <Link to={'/explore'} activeClassName="header__nav-link--active" className="header__nav-link">Explore</Link>
+        </li>
+      </nav>
+    </div>
   </header>
 );
 
