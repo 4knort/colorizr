4a3cdc8c706ad75829d883cf5a152be85b1f9e3c
Nort <4knort@gmail.com>
2017-03-16 11:40:01 +0200

    store, router are configured, deleted useless boilerplate lines

--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,19 @@
 import React, { PropTypes } from 'react';
-import { Route, IndexRoute } from 'react-router';
-import { HotRouter } from 'utils';
-
+import { Router, Route, IndexRoute } from 'react-router';
 import { IndexPage, ExplorePage, ModifyPage, ExportPage } from 'pages';
 import { App } from 'components';
 
 const AppRouter = ({ history }) => (
-  <HotRouter history={history}>
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={IndexPage} />
       <Route path="/explore" component={ExplorePage} />
       <Route path="/modify" component={ModifyPage} />
       <Route path="/export" component={ExportPage} />
     </Route>
-  </HotRouter>
+  </Router>
 );
 
 AppRouter.propTypes = { history: PropTypes.object.isRequired };
+
 export default AppRouter;
