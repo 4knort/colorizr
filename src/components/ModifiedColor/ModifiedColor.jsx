0a32d89267df0407e3023cc12255da034e1d6752
Nort <4knort@gmail.com>
2017-03-17 19:06:30 +0200

    deleted useless comment

--- src/components/ModifiedColor/ModifiedColor.jsx
+++ src/components/ModifiedColor/ModifiedColor.jsx
@@ -2,25 +2,6 @@ import React, { Component, PropTypes } from 'react';
 
 import './modified-color.scss';
 
-// const ModifiedColor = ({ color, onClickAddColor, isAdded }) => {
-//   const buttonFunctionality = isAdded ? 'Remove' : 'Add'
-//   return (
-//     <div className="modified-color">
-//       <button className="modified-color__add" onClick={() => { onClickAddColor(isAdded, color); }}>{buttonFunctionality}</button>
-//       <div className="modified-color__item" style={{ backgroundColor: color }} />
-//       <p className="modified-color__value">{color}</p>
-//     </div>
-//   );
-// };
-
-// ModifiedColor.propTypes = {
-//   color: PropTypes.string.isRequired,
-//   onClickAddColor: PropTypes.func.isRequired,
-//   isAdded: PropTypes.bool.isRequired,
-// };
-
-// export default ModifiedColor;
-
 export default class ModifiedColor extends Component {
   componentDidMount() {
     this.props.getRandomModifyColor();
